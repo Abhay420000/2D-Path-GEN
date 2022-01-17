5ee286c8fb8fb4ca887284256e18e81028ff1c80
Abhay Kumar <69663532+Abhay420000@users.noreply.github.com>
2022-01-17 19:35:44 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from kivy.clock import Clock
 from kivy.graphics.instructions import InstructionGroup
 from kivy.core.window import Window
 from kivy.graphics import Line
+from kivy.graphics import Mesh
+from kivy.graphics import Color
+from kivy.graphics import Quad
 import random
 
 class BBG(Widget):
@@ -11,7 +14,7 @@ class BBG(Widget):
         super().__init__()
         
         self.pth = InstructionGroup()
-        self.w = 2#Path Segments' width
+        self.w = 4#Path Segments'(Grass,Line) width
         
         number_of_lines_in_path = random.randint(5,50)#Generating Random Amount Of Lines
         #print(number_of_lines_in_path)
@@ -20,16 +23,22 @@ class BBG(Widget):
         px = 0
         py = random.randint(int(Window.size[1]/3), int(Window.size[1]/2))
         
-        for i in range(0, number_of_lines_in_path+100):
+        for i in range(0, number_of_lines_in_path+75):
                         #Adding 100 in case if all line's(path segment's) become less
                         #than length of x-axis because of all line's length may differ due to angle.
-            pline = Line(points = [px,py,px+self.div,random.randint(int(Window.size[1]/3), int(Window.size[1]/2))], width = self.w)
+            pline = Line(points = [px,py,px+self.div,random.randint(int(Window.size[1]/3), int(Window.size[1]/2))], width = self.w, )
+            qds = Quad(points = pline.points+[pline.points[2],0,pline.points[0],0])
+            self.pth.add(Color(0.5,0.4,0.1))
+            self.pth.add(qds)
+            self.pth.add(Color(0,1,0))
             self.pth.add(pline)
+
+            #Storing This For Connecting Next Segment to it
             px = pline.points[2]
             py = pline.points[3]
-                        
+
         self.canvas.add(self.pth)
-        Clock.schedule_interval(self.animate_path, 0.01)
+        Clock.schedule_interval(self.animate_path, 0.0166)#60FPS
 
     def animate_path(self, _utk):
         #For Movement of all Path's Segments
@@ -42,32 +51,63 @@ class BBG(Widget):
                     p[2] -= 1
                     self.pth.insert(i, Line(points = p, width = self.w))
                     self.pth.remove(newL)
-
+            if (type(self.pth.children[i]) == type(Quad())):
+                if (self.pth.children[i].points[2] != 0):
+                    newL = self.pth.children[i]
+                    p = self.pth.children[i].points
+                    p[0] -= 1
+                    p[2] -= 1
+                    p[4] -= 1
+                    p[6] -= 1
+                    self.pth.insert(i, Quad(points = p))
+                    self.pth.remove(newL)
+        
         #Removing a path's segment when it leaves the screen
-        for i in range(0,self.pth.length()):
+                
+        for i in range(0,self.pth.length()):#Deleting Quad
+            if (type(self.pth.children[i]) == type(Quad())):
+                if (self.pth.children[i].points[2] <= 0):
+                    to_delete_Quad = self.pth.children[i]
+                    if (type(self.pth.children[i-1]) == type(Color())):
+                        self.pth.remove(self.pth.children[i-1])
+                    self.pth.remove(to_delete_Quad)
+                    break
+                                
+        for i in range(0,self.pth.length()):#Deleting Line
             if (type(self.pth.children[i]) == type(Line())):
                 if (self.pth.children[i].points[2] <= 0):
-                    newL = self.pth.children[i]
+                    old_Line = self.pth.children[i]
                     length = self.pth.children[i].points[2] - self.pth.children[i].points[0]
-                    self.pth.remove(newL)
-                    self.make_path(length)
+                    if (type(self.pth.children[i-1]) == type(Color())):
+                        self.pth.remove(self.pth.children[i-1])
+                    self.pth.remove(old_Line)
+                    self.make_path(length)#Adding new Segment
                     break
+
+        #print(self.pth.length())
     
     def make_path(self,length):
         """
-        For Adding new Lines(Path Segment)
+        For Adding new Road
         """
-        px = 0
+        #px = 0
+        #py = random.randint(int(Window.size[1]/3), int(Window.size[1]/2))
+        
         for i in range(self.pth.length()-1,-1,-1):
-            if (type(Line()) == type(self.pth.children[i])):
+            if (type(self.pth.children[i]) == type(Line())):
                 px = self.pth.children[i].points[2]
                 py = self.pth.children[i].points[3]
                 break
             
         ny = random.randint(int(Window.size[1]/3), int(Window.size[1]/2))
+
         newLine = Line(points = [px,py,px+length,ny], width = self.w)
+        qds = Quad(points = newLine.points+[newLine.points[2],0,newLine.points[0],0])
+
+        self.pth.add(Color(0.5,0.4,0.1))
+        self.pth.add(qds)
+        self.pth.add(Color(0,1,0))
         self.pth.add(newLine)
-            
 
 class BBApp(App):
     def build(self):
